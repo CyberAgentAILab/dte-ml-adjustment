3d0949800058e839cdd057dc474ff8d48b944b48
Undral Byambadalai <undralb@stanford.edu>
2024-05-27 12:35:00 +0900

    Update data pre-processing

--- data/data_pre_process.R
+++ data/data_pre_process.R
@@ -11,8 +11,10 @@ library(haven)
 ## https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN1/22633&version=1.1 
 ## Save 090113_TotWatDat_cor_merge_Price.dta file in data folder
 
+# Load original data
 original_data = read_dta("090113_TotWatDat_cor_merge_Price.dta")
 
+# Select variables and save as .csv file
 original_data %>% mutate(Y = jun07_3x+jul07_3x+aug07_3x+sep07_3x, D = treatment) %>%
   select(Y,D,jun06,jul06,aug06,sep06,oct06,nov06,dec06,jan07,feb07,mar07,apr07_3x,may07_3x) %>%
   write_csv("data_ferraroprice.csv")
