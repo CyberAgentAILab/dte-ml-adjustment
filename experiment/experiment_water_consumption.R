5263e88b59ed2a07079722eb6c1f11418a5b1f89
Undral Byambadalai <undralb@stanford.edu>
2024-05-27 13:53:10 +0900

    Update experiment_water_consumption.R

--- experiment/experiment_water_consumption.R
+++ experiment/experiment_water_consumption.R
@@ -25,21 +25,21 @@ cb_colors = brewer.pal(n = 8, name = "Dark2") # discrete colorblind palette
 ##------------------------------------------------------------------------------
 ## Load data
 ##------------------------------------------------------------------------------
-df = read.csv('./data/data_ferraroprice.csv') %>% 
+df = read.csv('../data/data_ferraroprice.csv') %>% 
   na.omit() %>%
   filter(D==3 | D==4) %>%
   mutate( D = as.factor( 1 * (D==3) )) %>%    ## treatment effect dummy
   data.frame()
 
 # # For analysis of T1 vs Control use the following code instead
-# df = read.csv('./data/data_ferraroprice.csv') %>% 
+# df = read.csv('../data/data_ferraroprice.csv') %>% 
 #   na.omit() %>%
 #   filter(D==1 | D==4) %>%
 #   mutate( D = as.factor( 1 * (D==1) )) %>%    ## treatment effect dummy
 #   data.frame()
 
 # # For analysis of T2 vs Control use the following code instead
-# df = read.csv('./data/data_ferraroprice.csv') %>% 
+# df = read.csv('../data/data_ferraroprice.csv') %>% 
 #   na.omit() %>%
 #   filter(D==2 | D==4) %>%
 #   mutate( D = as.factor( 1 * (D==2) )) %>%    ## treatment effect dummy
@@ -76,8 +76,8 @@ print(paste("Time spent:", end_time-start_time))
 ##------------------------------------------------------------------------------
 ## Plot PTE (simple)
 ##------------------------------------------------------------------------------
-y.max = max(max(res.ml.est$pte$cv975), max(res.ml.est$pte$cv975)) + 1e-5
-y.min = min(min(res.ml.est$pte$cv025), min(res.ml.est$pte$cv025)) - 1e-5
+y.max = max(max(res.ml.est$pte$cv975), max(res.ml.est$pte.ra$cv975)) + 1e-5
+y.min = min(min(res.ml.est$pte$cv025), min(res.ml.est$pte.ra$cv025)) - 1e-5
 
 ggplot(res.ml.est$pte, aes(vec.loc, pte) ) + 
   geom_bar( stat = "identity", color= cb_colors[1], fill=cb_colors[1]) +
@@ -99,15 +99,12 @@ ggplot(res.ml.est$pte, aes(vec.loc, pte) ) +
         legend.title = element_text(size = 16, face = "bold"),
         strip.text = element_text(size = 14, face = "bold"))
 
-ggsave("./result/FP_PTE_simple.pdf", width=5, height =3)  ## save 
+ggsave("../result/FP_PTE_simple.pdf", width=5, height =3)  ## save 
 
 
 ##------------------------------------------------------------------------------
 ## Plot PTE (adjusted)
 ##------------------------------------------------------------------------------
-y.max = max(max(res.ml.est$pte.ra$cv975), max(res.ml.est$pte.ra$cv975)) + 1e-5
-y.min = min(min(res.ml.est$pte.ra$cv025), min(res.ml.est$pte.ra$cv025)) - 1e-5
-
 ggplot(res.ml.est$pte.ra, aes(vec.loc, pte) ) + 
   geom_bar( stat = "identity", color= cb_colors[3], fill=cb_colors[3]) +
   geom_errorbar(aes(ymin = cv025, 
@@ -128,7 +125,7 @@ ggplot(res.ml.est$pte.ra, aes(vec.loc, pte) ) +
         legend.title = element_text(size = 16, face = "bold"),
         strip.text = element_text(size = 14, face = "bold"))
 
-ggsave("./result/FP_PTE_adj.pdf", width=5, height =3) ## save
+ggsave("../result/FP_PTE_adj.pdf", width=5, height =3) ## save
 
 ##------------------------------------------------------------------------------
 ## Plot DTE (simple)
@@ -157,7 +154,7 @@ ggplot() +
         legend.title = element_text(size = 16, face = "bold"),
         strip.text = element_text(size = 14, face = "bold"))
 
-ggsave("./result/FP_DTE_simple.pdf", width=5, height =3)  ## save 
+ggsave("../result/FP_DTE_simple.pdf", width=5, height =3)  ## save 
 
 ##------------------------------------------------------------------------------
 ## Plot DTE (adjusted)
@@ -184,5 +181,5 @@ ggplot() +
         legend.title = element_text(size = 16, face = "bold"),
         strip.text = element_text(size = 14, face = "bold"))
 
-ggsave("./result/FP_DTE_adj.pdf", width=5, height =3)  ## save 
+ggsave("../result/FP_DTE_adj.pdf", width=5, height =3)  ## save 
 
